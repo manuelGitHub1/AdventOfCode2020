09787b6fd4bb8a7a353561b139115215a6b11fd6
manuel.breul <manuel@chrono24.com>
2020-12-02 21:36:42 +0100

    Day02: Second Part solved

--- src/main/java/Day02.java
+++ src/main/java/Day02.java
@@ -5,38 +5,54 @@ public class Day02 {
 
    public static void main( String[] args ) {
       final List<String> strings = Util.fileAsStrings("src/main/resources/day02/input.txt");
-      final long count = strings.stream().map(Day02::stringToObject).filter(Day02::validate).count();
-      System.out.printf("Found %s invalid passwords", count);
+      // first part
+      final long count = strings.stream().map(Day02::stringToObject).filter(Day02::validateOldRule).count();
+      System.out.printf("Part1: Found %s valid passwords", count);
+      // second part
+      final long count2 = strings.stream().map(Day02::stringToObject).filter(Day02::validateNewRule).count();
+      System.out.printf("\nPart2: Found %s valid passwords", count2);
    }
 
    private static RuleAndPassword stringToObject( String input ) {
       assert input != null;
-      final String[] split = input.split(" ");
-      assert split.length == 3;
+      final String[] split = prepareRule(input, " ", 3);
       final RuleAndPassword rap = new RuleAndPassword();
-      rap.setCount(split[0]);
+      rap.setRule(split[0]);
       rap.setCharacter(split[1].substring(0, 1));
       rap.setPassword(split[2]);
       return rap;
    }
 
-   private static boolean validate( RuleAndPassword rap ) {
-      final String[] split = rap.getCount().split("-");
-      assert split.length == 2;
+   private static boolean validateNewRule( RuleAndPassword rap ) {
+      final String[] split = prepareRule(rap.getRule(), "-", 2);
+      int firstIndex = Integer.parseInt(split[0]);
+      int secondIndex = Integer.parseInt(split[1]);
+      int lookUpChar = rap.getCharacter().charAt(0);
+      final String password = rap.getPassword();
+      final boolean firstLookupMatches = password.charAt(firstIndex - 1) == lookUpChar;
+      final boolean secondLookupMatches = password.charAt(secondIndex - 1) == lookUpChar;
+      return firstLookupMatches != secondLookupMatches;
+   }
+
+   private static String[] prepareRule( String rule, String s, int i ) {
+      final String[] split = rule.split(s);
+      assert split.length == i;
+      return split;
+   }
+
+   private static boolean validateOldRule( RuleAndPassword rap ) {
+      final String[] split = prepareRule(rap.getRule(), "-", 2);
       int min = Integer.parseInt(split[0]);
       int max = Integer.parseInt(split[1]);
-
       int lookUpChar = rap.getCharacter().charAt(0);
-
       final long appearances = rap.getPassword().chars().filter(c -> c == lookUpChar).count();
-
       return appearances >= min && max >= appearances;
    }
 
 
    private static class RuleAndPassword {
 
-      String _count;
+      String _rule;
       String _character;
       String _password;
 
@@ -44,24 +60,24 @@ public class Day02 {
          return _character;
       }
 
-      public String getCount() {
-         return _count;
-      }
-
       public String getPassword() {
          return _password;
+      }
+
+      public String getRule() {
+         return _rule;
       }
 
       public void setCharacter( String character ) {
          _character = character;
       }
 
-      public void setCount( String count ) {
-         _count = count;
-      }
-
       public void setPassword( String password ) {
          _password = password;
+      }
+
+      public void setRule( String rule ) {
+         _rule = rule;
       }
    }
 
